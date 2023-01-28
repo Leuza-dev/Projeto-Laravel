69816ca8ad67b2e84f448aef788a0f9d61d62215
Leuza Lima <leuzadev@gmail.com>
2023-01-28 17:06:19 -0300

    ajuste de logo

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -20,7 +20,7 @@
   <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#"><img src="img/logo-eventos.png" alt="logo" style="max-width: 50px"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
               <span class="navbar-toggler-icon"></span>
             </button>
